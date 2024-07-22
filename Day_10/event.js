eddca5bb182fbd064a5ea19a65479238272a8932
Sai Ganesh Ratnala <125180139+saiganesh1976@users.noreply.github.com>
2024-07-22 18:24:13 +0000

    container config done

---
+++ Day_10/event.js
@@ -0,0 +1,8 @@
+let paragraph = document.getElementsByClassName("paragraph");
+console.log(paragraph);
+let btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+  paragraph[0].textContent = "lorem ddbvjhbdvhjb";
+});
+
+let image=document.getElementById('image')
