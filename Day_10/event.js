0d4dc8ee60da2876e8183ee9eccc8e1a3cfb14a8
Sai Ganesh Ratnala <125180139+saiganesh1976@users.noreply.github.com>
2024-07-25 18:00:29 +0000

    container config done

--- Day_10/event.js
+++ Day_10/event.js
@@ -1,8 +1,50 @@
 let paragraph = document.getElementsByClassName("paragraph");
 console.log(paragraph);
 let btn = document.getElementById("btn");
-btn.addEventListener("click", () => {
+btn.addEventListener("click", (e) => {
   paragraph[0].textContent = "lorem ddbvjhbdvhjb";
 });
 
-let image=document.getElementById('image')
+// Button
+
+document.querySelector(".dbl").addEventListener("dbclick", (e) => {
+  document.getElementById("img").style.display = "none";
+});
+document.querySelector(".ck").addEventListener("click", (e) => {
+  document.getElementById("img").style.display = "block";
+});
+
+// Mouse hover
+
+let change_bg = document.getElementsByClassName("changeBg");
+console.log(change_bg);
+change_bg[0].addEventListener("mouseover", (e) => {
+  change_bg[0].style.backgroundColor = "yellow";
+});
+change_bg[0].addEventListener("mouseout", (e) => {
+  change_bg[0].style.backgroundColor = "white";
+});
+
+// KeyBoard Input
+
+let userInput = document.getElementById("userInput");
+
+userInput.addEventListener("keypress", (e) => {
+  console.log(` userInput using KeyPress: ${userInput.value}`);
+});
+
+userInput.addEventListener("keyup", (e) => {
+  console.log(`userInput using KeyUp: ${userInput.value}`);
+});
+
+// Form Events
+let form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+  formData.forEach((value, key) => {
+    console.log(`${key}:${value}`);
+  });
+});
